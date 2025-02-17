a0617a105a7514e959e9775c5abb4dc0d880520d
katerinka228 <varfolomeewa.catherine@yandex.ru>
2025-02-17 18:14:02 +0300

    second commit

--- src/components/ElementList/index.tsx
+++ src/components/ElementList/index.tsx
@@ -5,23 +5,27 @@
 //
 //
 //
-// export default function ElementList({name, isPacked}:Props) {
-//     // let nameElement = name;
-//     // if (isPacked)
-//     // {
-//     //     nameElement = name + " ✅";
-//
-//
-//     return <li> {isPacked ?  name + " ✅" : name}</li>
-//     }
-//
-//     // return <li>{nameElement}</li>
-import Style from "../StateIndex/stataIndex.module.css";
-import classNames from 'classnames';
-interface Props {
-    name: string;
-    isPacked: boolean;
-}
-export default function ElementList({name, isPacked}: Props) {
-    return <li className={classNames(Style.StateIndex, !isPacked && Style.StateIndexFalse)}>{isPacked ? name + " ✅" : name}</li>
-}
+export default function ElementList({name, isPacked}:Props) {
+    // let nameElement = name;
+    // if (isPacked)
+    // {
+    //     nameElement = name + " ✅";
+
+
+    return <li> {isPacked ?  name + " ✅" : name}</li>
+    }
+
+    // return <li>{nameElement}</li>
+
+
+
+//допилить
+// import Style from "../StateIndex/stataIndex.module.css";
+// import classNames from 'classnames';
+// interface Props {
+//     name: string;
+//     isPacked: boolean;
+// }
+// export default function ElementList({name, isPacked}: Props) {
+//     return <li className={classNames(Style.StateIndex, !isPacked && Style.StateIndexFalse)}>{isPacked ? name + " ✅" : name}</li>
+// }
